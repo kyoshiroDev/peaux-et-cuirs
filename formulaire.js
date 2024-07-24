a96a35ca9098c195160d113159a0f5f4da17fbbf
greg <tahir.gregory.dev@gmail.com>
2024-07-24 11:23:19 +0200

    mise en forme du formulaire contact

---
+++ formulaire.js
@@ -0,0 +1,30 @@
+// Récupére le formulaire
+
+// ajoute l'événement de soumission
+
+// empêche le rechargement de la page
+
+// Reset message error
+
+// Récupérer l'ensemble des inputs du formulaire
+
+// dire que le formulaire est valide
+
+// boucler sur les inputs
+
+// récupérer la valeur de l'input
+
+// récupérer le type de l'input
+
+// switch type (chaque élément vérifie son propre type)
+
+// Si le type est invalide,
+// il y a une erreur (input invalide)
+// On créer une div avec la class error
+
+// Si le formulaire est valide, on le soumet
+
+// Fonction text valide
+// Fonction email valide
+// Fonction number valide
+// Fonction tel valide
--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const burgerButton = document.querySelector(".box");
 const navBar = document.querySelector("nav");
+const linkNavBar = document.querySelectorAll(".nav-link");
 
 burgerButton.addEventListener("click", () => {
 	burgerButton.classList.toggle("active");
 	navBar.classList.toggle("open");
+});
+
+linkNavBar.forEach((link) => {
+	link.addEventListener("click", () => {
+		if (navBar.classList.contains("open")) {
+			navBar.classList.remove("open");
+			burgerButton.classList.remove("active");
+		}
+	});
 });
